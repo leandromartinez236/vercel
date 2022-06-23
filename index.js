00b9e3550f5b7519de354f462916e22f6b3a476f
leandro <martinezleandro2004@gmail.com>
2022-06-23 13:13:31 -0300

    cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 require("dotenv").config();
 require("./api/asociations/asociation");
-
+const cors = require("cors");
 const userRoutes = require("./api/routes/user.routes");
 const ticketRoutes = require("./api/routes/ticket.routes");
 
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/tickets", ticketRoutes);
